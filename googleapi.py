1e4b44c26803efdd611c8b5d42ce711f0c741bc9
rakulmaria <rakulmaria@hotmail.com>
2026-03-10 14:08:18 +0100

    fix end of files

--- googleapi.py
+++ googleapi.py
@@ -4,33 +4,63 @@ import os
 import requests
 
 API_KEY = os.getenv("MAPS_PLATFORM_API_KEY")
-# FIELD_MASK = 'places.id,places.types,places.displayName'
+HEADERS = {
+    "Content-Type": "application/json",
+    "X-Goog-Api-Key": API_KEY,
+    "X-Goog-FieldMask": "*",  # what i want to be included in the respond
+}
+
+# FIELD_MASK = 'places.id,places.types,places.displayName
+
+def search_nearby(lat, lon, radius):
+    url = "https://places.googleapis.com/v1/places:searchNearby"
+
+    data = {
+        "includedTypes": ["restaurant"],
+        "locationRestriction": {
+            "circle": {
+                "center": {
+                    "latitude": lat,
+                    "longitude": lon,
+                },
+                "radius": radius,
+            }
+        },
+    }
+
+    response = requests.post(url, json=data, headers=HEADERS)
+    
+    if response.status_code == 200:
+        print(f"Response OK: {response.status_code}")
+    else:
+        print(f"! ERROR: {response.status_code}")
+        print(f"{response.text}")
+
+    data = response.json()
+
+    return data.get("places", [])
+
 
 
 def find_aggregated_places():
     url = "https://areainsights.googleapis.com/v1:computeInsights"
 
-    headers = {
-        "Content-Type": "application/json",
-        "X-Goog-Api-Key": API_KEY,
-        "X-Goog-FieldMask": "*",  # what i want to be included in the respond
-    }
-
     data = {
         "insights": ["INSIGHT_COUNT", "INSIGHT_PLACES"],
         "filter": {
             "locationFilter": {
-                "region": {"place": "places/ChIJIz2AXDxTUkYRuGeU5t1-3QQ"}
+                "region": {"place": "places/ChIJIz2AXDxTUkYRuGeU5t1-3QQ"} # copenhagen ID
             },
             "typeFilter": {"includedTypes": "restaurant"},
         },
     }
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, json=data, headers=HEADERS)
 
     if response.status_code == 200:
         print(f"Response OK: {response.status_code}")
     else:
         print(f"! ERROR: {response.status_code}")
+
 
     return json.loads(response.text)
 
@@ -39,18 +69,11 @@ def find_place(query):
     # Define the API endpoint
     url = "https://places.googleapis.com/v1/places:searchText"
 
-    # Define the headers
-    headers = {
-        "Content-Type": "application/json",
-        "X-Goog-Api-Key": API_KEY,
-        "X-Goog-FieldMask": "*",  # what i want to be included in the respond
-    }
-
     # Define the data payload for the POST request
     data = {"textQuery": query}
 
     # Make the POST request
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, json=data, headers=HEADERS)
 
     # Check if the request was successful
     if response.status_code == 200:
