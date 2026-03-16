86ea7a0ea81c628ae5ab9707946db9838d969284
rakulmaria <rakulmaria@hotmail.com>
2026-03-16 13:03:59 +0100

    feat: add logger logic for saving logs to a file

--- src/logger.py
+++ src/logger.py
@@ -3,8 +3,10 @@ import logging
 
 def setup_logger():
     logging.basicConfig(
-        level=logging.DEBUG,     # see everything
-        # level=logging.INFO,    # only high-level progress
+        # filename='example.log', # enables writing logs to a file
+        # encoding='utf-8',       # remember encoding
+        # level=logging.DEBUG,     # see everything
+        level=logging.INFO,    # only high-level progress
         # level=logging.WARNING, # silent unless something goes wrong
         format="%(asctime)s [%(levelname)s] %(message)s",
     )
